d57d4eaf4f7ec05ffca56c7efea820b242e8cc36
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-09-06 18:43:46 -0300

    Adding the back-end template from Spring initializr

---
+++ meetingrooms/src/test/java/kannon/aaron/meetingrooms/meetingrooms/MeetingroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package kannon.aaron.meetingrooms.meetingrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
