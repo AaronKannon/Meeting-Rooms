ebc80c3fca5a29c043129449da68d4a6f0db4ade
AaronKannon <wellingtonbmoraes@hotmail.com>
2021-09-07 21:20:14 -0300

    The bugs, which were created because of the change to the file database, were fixed

--- meetingrooms/src/main/java/kannon/aaron/meetingrooms/meetingrooms/model/Room.java
+++ meetingrooms/src/main/java/kannon/aaron/meetingrooms/meetingrooms/model/Room.java
@@ -17,7 +17,7 @@ import java.util.Date;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Room {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @Column(nullable = false)
     String name;
